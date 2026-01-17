6bd964c4273afb8cdd171fb0385db66990733edc
AdrianVG-13 <adrianvazq13@gmail.com>
2026-01-17 11:46:40 -0600

    Primera Subida del archivo de ejemplo Python

---
+++ ejemplo.py
@@ -0,0 +1,15 @@
+# COMENTARIOS:
+# Son una descripción del programa o código 
+# Los comentarios los om,ite o no son tomados en cuenta python
+
+# Ejemplo
+print("Adrian") # Nombre
+print('Vazquez') # Primer Apellido 
+print('Garcia') # Segundo Apellido
+
+print () #Línea en blanco
+
+print(5+2)  # Suma
+print(10-2) # Resta
+print(3*4)  # Multiplicación
+print(8/2)  # División
